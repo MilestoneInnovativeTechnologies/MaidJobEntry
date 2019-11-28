ff59630aa3e1db4c6f4802667d5e74b78154c64c
Firose Hussain <github@firumon.com>
2019-11-28 10:14:57 +0530

    Service Provider Created

---
+++ src/MaidJobEntryServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Milestone\MaidJobEntry;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Event;
+
+class MaidJobEntryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
