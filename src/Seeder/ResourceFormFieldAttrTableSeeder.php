7324313cf19f54376108531f9eaf40bdd276df0a
Firose <firumon@github.com>
2019-11-28 15:08:37 +0530

    MAid Job entry fields made inline

--- src/Seeder/ResourceFormFieldAttrTableSeeder.php
+++ src/Seeder/ResourceFormFieldAttrTableSeeder.php
@@ -23,6 +23,10 @@ class ResourceFormFieldAttrTableSeeder extends Seeder
             ->create([	'id' => '556405', 	'form_field' => '556101', 	'name' => 'inline', 	'value' => '4', 												])
             ->create([	'id' => '556406', 	'form_field' => '556102', 	'name' => 'inline', 	'value' => '4', 												])
             ->create([	'id' => '556407', 	'form_field' => '556103', 	'name' => 'inline', 	'value' => '4', 												])
+            ->create([	'id' => '556408', 	'form_field' => '556108', 	'name' => 'inline', 	'value' => '4', 												])
+            ->create([	'id' => '556409', 	'form_field' => '556109', 	'name' => 'inline', 	'value' => '4', 												])
+            ->create([	'id' => '556410', 	'form_field' => '556110', 	'name' => 'inline', 	'value' => '4', 												])
+            ->create([	'id' => '556411', 	'form_field' => '556111', 	'name' => 'inline', 	'value' => '4', 												])
         ;
         \DB::statement('set foreign_key_checks = ' . $_);
     }
